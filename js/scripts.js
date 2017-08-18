1e7bba0ada9d950ca390b0742acaaf3e6d359d5d
Nelly Cheruiyot <nellycheruiyot@gmail.com>
2017-08-18 13:16:02 +0300

    Delete css folder and add another with bootstrap.css Add code to index.html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
-$(document).ready(function(event) {
-  $(".menu").click(function() {
-    $(".menulist").toggle();
-    //$(".walrus-hidden").toggle();
+var add = function(number1, number2) {
+  return number1 + number2;
+};
 
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
     event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
   });
+  var favoriteColor = $("#color").val();
 });
-//  $(".scroll").click(function(event){
-//    event.preventDefault();
-  //  $('html,body').animate({scrollTop:$(this.hash).offset().top},1200);
